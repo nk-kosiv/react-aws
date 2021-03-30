c976b3a876ab2424cdf5b02cc80e57ed908e3d15
Nazar Khrushch <mac15pro@Nazar-Khrushch.local>
2021-03-30 15:41:17 +0300

    added boilerplate of react app to the component

---
+++ src/components/Content/Component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Welcome from "./Welcome";
+import HotelInfo from "./HotelInfo";
+
+const Content = () => {
+  return (
+    <main id="wrapper">
+      <Welcome />
+      <HotelInfo />
+    </main>
+  );
+};
+
+export default Content;
